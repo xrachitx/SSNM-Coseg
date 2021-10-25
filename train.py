267383d11d37fa240e23556a1060b1b52f801253
xrachitx <rachit18302@iiitd.ac.in>
2021-10-26 02:11:09 +0530

    deducing

--- train.py
+++ train.py
@@ -19,9 +19,12 @@ def train(net, device, q, log_txt_file, val_datapath, models_train_best, models_
         # print(img.shape)
         net.zero_grad()
         img, cls_gt, mask_gt = img.to(device), cls_gt.to(device), mask_gt.to(device)
+        print("Forward")
         pred_cls, pred_mask = net(img)
         # exit()
+        print("Loss Computation")
         all_loss, m_loss, c_loss, s_loss = loss(pred_mask, mask_gt, pred_cls, cls_gt)
+        print("Backward")
         all_loss.backward()
         epoch_loss = all_loss.item()
         m_l = m_loss.item()
@@ -71,4 +74,4 @@ def train(net, device, q, log_txt_file, val_datapath, models_train_best, models_
         if epoch % lr_de_epoch == 0:
             optimizer = Adam(net.parameters(), lr/2, weight_decay=1e-6)
             lr = lr / 2
-        del img, cls_gt, mask_gt
+        del img, cls_gt, mask_gt,all_loss, m_loss, c_loss, s_loss,pred_cls, pred_mask 
